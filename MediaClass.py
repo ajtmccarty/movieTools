4a63e0a422007f21fc0ad402c16fcc3e5dea1bb8
AJTM <aaronjohntmccarty@gmail.com>
2016-05-15 17:45:54 -0500

    Add path and rawName to MediaClass.Media

--- MediaClass.py
+++ MediaClass.py
@@ -1,11 +1,33 @@
+
+'''
+class to store various pieces of information regarding a given
+piece of Media
+path = file path
+rawName = file name w/o file extensions
+title = title
+year = year of release
+mType = type of media
+link = url to imdb page for media
+actors = list of actors
+'''
 class Media:
-    def __init__(self,title="",year=None,mType='',link="",actors=[]):
+    def __init__(self,path,rawName="",title="",year=None,mType='',link="",actors=[]):
         self.mDict = {}
+        self.mDict["path"] = path
+        self.mDict["rawName"] = rawName
         self.mDict["title"] = title
         self.mDict["year"] = year
         self.mDict["type"] = mType
         self.mDict["link"] = link
         self.mDict["actors"] = actors
+
+    def addPath(self,newPath):
+        self.mDict["path"] = newPath
+        return
+
+    def addRawName(self,rawName):
+        self.mDict["rawName"] = rawName
+        return
 
     def addTitle(self, title):
         self.mDict["title"] = str(title)
@@ -26,6 +48,12 @@ class Media:
     def addActor(self, actor):
         self.mDict["actors"].append(actor)
         return
+
+    def getPath(self):
+        return self.mDict["path"]
+
+    def getRawName(self):
+        return self.mDict["rawName"]
 
     def getTitle(self):
         return self.mDict["title"]

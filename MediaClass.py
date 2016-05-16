5292ced6b13bc4cf47c239c5f360b706b1aee2de
AJTM <aaronjohntmccarty@gmail.com>
2016-05-16 10:57:02 -0500

    Add another extraction method extractFromName

--- MediaClass.py
+++ MediaClass.py
@@ -1,3 +1,7 @@
+import re
+import logging
+import sys
+
 #list of common movie file extensions
 MOVIE_FILE_EXTENSIONS = ['avi','mov','wmv','mp4','m4p','m4v','mpg','mpeg', \
                          'mpe','mpv']
@@ -5,7 +9,8 @@ MOVIE_FILE_EXTENSIONS = ['avi','mov','wmv','mp4','m4p','m4v','mpg','mpeg', \
 '''
 class to store various pieces of information regarding a given
 piece of Media
-title = title
+rawTitle = actual title of movie, including chars illegal in file names
+fTitle = the fTitle is the title used in the file name, w/o illegal chars
 year = year of release
 mType = type of media
 link = url to imdb page for media
@@ -18,7 +23,8 @@ children = tree children of this Media Object, such as
 '''
 class Media:
     def __init__(self,absolutePath=""):
-        self.title = ""
+        self.rawTitle = ""
+        self.fTitle = ""
         self.year = None
         self.mType = ""
         self.link = ""
@@ -29,6 +35,7 @@ class Media:
         self.children = []
         if absolutePath:
             self.extractFromPath(absolutePath)
+            self.extractFromFName()
 
     '''
     INPUT: an absolute path to a file
@@ -55,61 +62,87 @@ class Media:
             self.fExt = ""
         return
 
-    def extractFromRawName(self):
-        pass
+    '''
+    INPUT: None, but runs on the filename of the Media Object
+    OUTPUT: tries to extract the title and year from the file name
+    '''
+    def extractFromFName(self):
+        if self.fName == "":
+            return
+        hasPar = self.fName.count("(")
+        #if there is no parenthesis, we assume there is no year
+        if not hasPar:
+            self.fTitle = self.fName
+        [title,year] = self.fName.rsplit("(")
+        #check if year is 4 digits followed by a parenthesis
+        if re.match("[0-9]{4}\)",year):
+            self.year = int(year[:4])
+            self.fTitle = title
+        else:
+            logging.warning("Malformed file name %s",self.fName)
+            logging.warning("Has a parenthesis, but does not appear to contain a year")
+            sys.stderr.write("Encountered a strange file name "+self.fName+"\n")
+            sys.stderr.write("See log file for more information\n")
 
-    def addTitle(self, title):
-        self.title = str(title)
+    def addRawTitle(self, title):
+        self.rawTitle = title
         return
+
+    def getRawTitle(self):
+        return self.rawTitle
+
+    def addFTitle(self, title):
+        self.fTitle = title
+        return
+
+    def getFTitle(self):
+        return self.fTitle
 
     def addYear(self, year):
         self.year = int(year)
         return
 
+    def getYear(self):
+        return self.year
+
     def addType(self,mType):
         self.mType = mType
         return
+
+    def getType(self):
+        return self.mType
 
     def addLink(self, link):
         self.link = str(link)
         return
 
+    def getLink(self):
+        return self.link
+
     def addActor(self, actor):
         self.actors.append(actor)
         return
+
+    def getActors(self):
+        return self.actors
 
     def addFDir(self,fDir):
         self.fDir = fDir
         return
 
+    def getFDir(self):
+        return self.fDir
+
     def addFName(self,fName):
         self.fName = fName
         return
+
+    def getFName(self):
+        return self.fName
 
     def addFExt(self,fExt):
         self.fExt = fExt
         return
 
-    def getFDir(self):
-        return self.fDir
-
-    def getFName(self):
-        return self.fName
-
     def getFExt(self):
         return self.fExt
-
-    def getTitle(self):
-        return self.title
-
-    def getYear(self):
-        return self.year
-
-    def getType(self):
-        return self.mType
-
-    def getLink(self):
-        return self.link
-
-    def getActors(self):
-        return self.actors
